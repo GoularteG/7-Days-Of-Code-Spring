7b02f8a2634c4eb4bc8222a8e442d8c3f111e16e
Gustavo <goulartegustavo20@gmail.com>
2024-08-04 12:26:18 -0300

    " 2/7: Implementação do Modelo e do Repositório"

---
+++ demo/src/main/java/_daysofcode/demo/model/SuperHeroisRepository.java
@@ -0,0 +1,7 @@
+package _daysofcode.demo.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SuperHeroisRepository extends JpaRepository<SuperHerois,Long> {
+
+}
---
+++ demo/src/main/resources/V1__create-table-supers.sql
@@ -0,0 +1,14 @@
+
+create table super herois(
+
+    id bigint not null auto_increment,
+    nome varchar(100) not null,
+    poder varchar(100) not null ,
+    genero varchar(100) not null ,
+    classificacao varchar(100) not null,
+    idade varchar(100) not null,
+    paisDeOrigem varchar(100) not null
+
+    primary key(id)
+
+);
